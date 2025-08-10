8ecc1246c34c08a8408c671d28e2c79d04db0272
khizar925 <khizarqamar06@gmail.com>
2025-08-10 22:23:24 +0500

    All files added

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,16 +1,13 @@
 import express from "express";
-import pool from "../db.js";
 
 const router = express.Router();
 
 // GET /routes/interviews - fetch all interviews
 router.get("/", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM interviews");
-    res.status(200).json({ success: true, data: result.rows });
+    res.status(200).json({message : "succesfull"})
   } catch (error) {
-    console.error("Error fetching interviews:", error.message);
-    res.status(500).json({ success: false, message: "Internal server error" });
+    res.status(400).json({message : "Unsuccessful"})
   }
 });
 
