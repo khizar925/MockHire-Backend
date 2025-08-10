3b0a8ccdb85eceef6f9025ebccf39b5aa16d925b
khizar925 <khizarqamar06@gmail.com>
2025-08-10 22:46:41 +0500

    Supabase added

--- routes/interviews.js
+++ routes/interviews.js
@@ -1,14 +1,20 @@
 import express from "express";
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const router = express.Router();
 
 // GET /routes/interviews - fetch all interviews
 router.get("/", async (req, res) => {
-  try {
-    res.status(200).json({message : "succesfull"})
-  } catch (error) {
-    res.status(400).json({message : "Unsuccessful"})
-  }
+  const { data, error } = await  supabase.from('interviews').select('*').order("id", {ascending: true});
+  if (error) return res.json(400).json({error : error.message})
+  res.status(200).json({data: data})
+
 });
 
 export default router;
